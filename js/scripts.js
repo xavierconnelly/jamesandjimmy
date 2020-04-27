a754e2c9a2f7aaad7a538095eeb0ac54cd8942b5
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2020-04-28 09:52:54 +1000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,3 @@
-// Load Content with AJAX
-$(document).ready(function() {
-  $('footer').load('https://jamesandjimmy.com/bottomnav.html');
-  $('#jamesnews').load('https://jamesandjimmy.com/news.txt');
-  $('#jamesbio').load('https://jamesandjimmy.com/about.txt');
-  $('#jamescontact').load('https://jamesandjimmy.com/contact.txt');
-});
-
-// Mobile Slideshow
-$("#slideshow > div:gt(0)").hide();
-	setInterval(function() {
-		$('#slideshow > div:first')
-		  .fadeOut(1000)
-		  .next()
-		  .fadeIn(1000)
-		  .end()
-		  .appendTo('#slideshow');
-		}, 3000
-);
-
 // Info/About Section 
 function openLeft() {
 	document.getElementById("grid-container").style.left = "0";
